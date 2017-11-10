5a5b9e0f546846d500c207f1bd8a762c8708a5e0
Eric Kim <ericpaulkim@gmail.com>
2017-11-09 17:17:01 -0800

    camera + basic movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Vector3 offset;
+	public GameObject toFollow;
+
+	void FixedUpdate () 
+	{
+		this.transform.position = toFollow.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float movementSpeed;
+
+	private Rigidbody2D rb;
+	private CircleCollider2D col;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		col = GetComponent<CircleCollider2D> ();
+	}
+
+	void Update ()
+	{
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		float moveVertical = Input.GetAxisRaw ("Vertical");
+
+		rb.velocity = new Vector2 (moveHorizontal * movementSpeed, moveVertical * movementSpeed);
+	
+	}
+}
